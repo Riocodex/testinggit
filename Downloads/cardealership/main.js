54651e98e28dbf785d0d0f868d27e153f36c1586
Riocodex <onwukachibike@gmail.com>
2024-05-27 21:13:44 +0200

    updated

---
+++ Downloads/cardealership/main.js
@@ -0,0 +1,8 @@
+function toggleNavbar() {
+    const navbarLinks = document.getElementById('navbar-links');
+    if (navbarLinks.style.display === 'flex') {
+        navbarLinks.style.display = 'none';
+    } else {
+        navbarLinks.style.display = 'flex';
+    }
+}
